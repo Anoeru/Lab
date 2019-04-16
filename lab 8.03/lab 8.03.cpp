dbec3b714a6f34eb2a8cc299da5930720913361f
anoeru <47786730+Anoeru@users.noreply.github.com>
2019-04-16 14:38:01 +0300

    Add files via upload

---
+++ lab 8.03/lab 8.03.cpp
@@ -0,0 +1,26 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <queue>
+using namespace std;
+
+
+int main()
+{
+	priority_queue<int> myQueue;
+	int n;
+
+	for (int i=0; i < 5; i++)
+	{
+		cout << "enter car number ";
+		cin >> n;
+		myQueue.push(n);
+	}
+
+	cout << "number of elements: " << myQueue.size() << endl;
+	cout << "here they are: \n";
+	while (!myQueue.empty()) {
+		cout << myQueue.top() << "\n";
+		myQueue.pop();
+	}
+	return 0;
+}
